19f252e3fb25dfbc14b0ed1ce1b3f011a36bf846
xmaslottery <150428169+xmaslottery@users.noreply.github.com>
2023-11-29 14:33:48 +0100

    Added control to number of total prizes via cookie

--- js/start.js
+++ js/start.js
@@ -17,8 +17,8 @@ let script6 = document.createElement("script");
 script6.src = "js/snow3d.js";
 body.appendChild(script6);
 if(!checkCookieExists("batch")) document.cookie = "batch=4";
+if(!checkCookieExists("totalprizes")) document.cookie = "totalprizes=30";
 
-document.cookie = "totalprizes=30";
 
 
 
